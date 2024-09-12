64c8a2e7123f6ee570d003429214d43bd3eac3d3
Gediminas Luksevicius <gediminas30@yahoo.ie>
2024-09-12 21:13:50 +0000

    Add main function

--- run.py
+++ run.py
@@ -36,3 +36,16 @@ def get_sales_data():
     return sales_data
 
 
+def validate_data(values):
+    """
+    Validates that input data contains exactly 3 values are integers.
+    """
+    try:
+        [int(value) for value in values]
+        if len(values) != 3:
+            raise ValueError(f"Exactly 3 values required, you provided {len(values)}")
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
+
+    return True
