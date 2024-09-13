b8d3b2b73dfc7247cb44e9ebac2c111260be6478
Gediminas Luksevicius <gediminas30@yahoo.ie>
2024-09-13 20:08:07 +0000

    Change spreadsheet name to spreadsheet ID

--- run.py
+++ run.py
@@ -2,7 +2,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
-# Define Google Sheets API scope and credentials
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -12,7 +11,7 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('car_parts_sale')
+SHEET = GSPREAD_CLIENT.open_by_key('1_wsutQEMeso8AFcV2-I9BJZfBTwSbN1P3jpc7tCIZ9w')
 
 
 def get_sales_data():
